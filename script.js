48c91e22599e712bffb586bd557f2889ff906c88
Oceanside18 <Mya.Mccarthy@edu.sait.ca>
2024-11-18 12:30:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 console.log ('insane in the menbrane');
+const ol = document.getElementById("grid");
 for (let i = 1; i <= 25; i++) {
     console.log(i)
     const li = document.createElement('li');
     console.log(li);
-    li.textContent = i;
-    console.log(li);
+    li.textContent = 'item number ${i}';
+    ol.appendChild(li);
 }
    
