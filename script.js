a4a15054d3081ffff6bdf8c580fb2ac005462cbf
Oceanside18 <Mya.Mccarthy@edu.sait.ca>
2024-11-18 12:26:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 console.log ('insane in the menbrane');
 for (let i = 1; i <= 25; i++) {
     console.log(i)
+    const li = document.createElement('li');
+    console.log(li);
+    li.textContent = i;
+    console.log(li);
 }
+   
